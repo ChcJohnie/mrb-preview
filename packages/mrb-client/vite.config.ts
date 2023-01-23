0bbac1bcf1028c2cbb0a911498f1e52766f34707
Jan Shanel <chcjohnie@gmail.com>
2023-01-23 23:51:48 +0100

    Add styling libs

--- packages/mrb-client/vite.config.ts
+++ packages/mrb-client/vite.config.ts
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import presetWind from '@unocss/preset-wind'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetWind(), presetAttributify(), presetIcons()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
