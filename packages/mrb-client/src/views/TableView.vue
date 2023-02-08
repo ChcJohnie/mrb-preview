34ea51e07cf3a4babd5e780780d47198d6d94a99
Jan Shanel <chcjohnie@gmail.com>
2023-02-08 13:51:30 +0100

    Add axios/vue-query
    No working api currently

--- packages/mrb-client/src/views/TableView.vue
+++ packages/mrb-client/src/views/TableView.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 import TestTable from '@/components/TestTable.vue'
 import ScrollColumn from '@/components/ScrollColumn.vue'
+
+// import { useQuery } from '@tanstack/vue-query'
+// import axios from 'axios'
+
+// const { isLoading, isError, data, error } = useQuery({
+//   queryKey: ['categories'],
+//   queryFn: () =>
+//     axios
+//       .get('http://liveresults.ch/WCF2022/long/config.json')
+//       .then((res) => res.data),
+//   select: (data) => data.categories,
+// })
 </script>
 
 <template>
   <div class="px-4 py-5 sm:px-6">
+    <!-- <div v-if="!isLoading && !isError">
+      <span v-for="category in data" :key="category.id">{{
+        category.name
+      }}</span>
+    </div> -->
+
     <ScrollColumn>
       <TestTable class="my-4" :name="'ABC'" :rows="10" />
       <TestTable class="my-4" :name="'ABC'" :rows="10" />
