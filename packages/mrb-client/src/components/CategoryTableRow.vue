fce40c464a029525f4e4a3e66adc38eebe7ff7bc
Jan Shanel <chcjohnie@gmail.com>
2023-02-19 13:28:47 +0100

    Improve table row render

--- packages/mrb-client/src/components/CategoryTableRow.vue
+++ packages/mrb-client/src/components/CategoryTableRow.vue
@@ -17,14 +17,23 @@ const bgColor = props.isEven ? 'bg-even' : 'bg-white'
 <template>
   <div
     :class="bgColor"
-    class="grid grid-flow-col auto-cols-fr px-3 py-1.5 rounded-lg"
+    class="grid table-row-grid gap-2 px-3 py-1.5 rounded-lg"
   >
-    <span>{{ data.rank }}.</span>
+    <span class="tabular-nums">{{ data.rank }}.</span>
     <span>{{ data.surname }} {{ data.firstName }}</span>
     <span>{{ data.si }}</span>
     <span>{{ data.club }}</span>
-    <span>{{ data.time }}</span>
-    <span v-if="data.loss">+ {{ data.loss }}</span>
+    <!-- TODO tabular-nums not nice override, special monospace font? -->
+    <span class="text-right tabular-nums">{{ data.time }}</span>
+    <span class="text-right tabular-nums" v-if="data.loss"
+      >+ {{ data.loss }}</span
+    >
     <span v-else></span>
   </div>
 </template>
+
+<style>
+.table-row-grid {
+  grid-template-columns: 2em 3fr 1fr 3fr 5em 5em;
+}
+</style>
