2e1f27d7f9e43e39ba13d0c29873ddb5c2e875c7
Jan Shanel <chcjohnie@gmail.com>
2023-02-20 16:46:52 +0100

    Update settings form

--- packages/mrb-client/src/components/TableSettings.vue
+++ packages/mrb-client/src/components/TableSettings.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { useSettingStore } from '@/stores/settings'
 const settingsStore = useSettingStore()
-const { setScrollType } = settingsStore
 </script>
 
 <template>
@@ -9,30 +8,75 @@ const { setScrollType } = settingsStore
     class="overflow-hidden shadow-lg border-t-4 bg-white mb-4 rounded-l-lg rounded-t border-header w-full md:w-1/4"
   >
     <div class="px-6 py-4 mt-4 mb-8">
-      <div class="inline-block bg-white border-black border-2">
-        <span>SCROLL: {{ settingsStore.scrollType }}</span>
-      </div>
-      <div class="flex gap-3">
-        <button
-          class="bg-white border-black border-2"
-          @click="setScrollType('page')"
-        >
-          Scroll by height
-        </button>
-        <button
-          class="bg-white border-black border-2"
-          @click="setScrollType('continues')"
-        >
-          Continues scroll
-        </button>
+      <h3>Result table settings</h3>
+      <form>
+        <div class="mb-6">
+          <h4>(Auto) Scrolling</h4>
+          <div class="flex items-center mb-2">
+            <input
+              v-model="settingsStore.scrollType"
+              id="scroll-page"
+              type="radio"
+              value="page"
+              name="scroll by page"
+              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="scroll-page"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Scroll by page</label
+            >
+          </div>
+          <div class="flex items-center mb-2">
+            <input
+              v-model="settingsStore.scrollType"
+              id="scroll-continues"
+              type="radio"
+              value="continues"
+              name="scroll continuesly"
+              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="scroll-continues"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Continues scroll</label
+            >
+          </div>
+          <div class="flex items-center mb-2">
+            <input
+              v-model="settingsStore.scrollType"
+              id="scroll-none"
+              type="radio"
+              value="none"
+              name="scroll manually"
+              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              for="scroll-none"
+              class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Manual scroll</label
+            >
+          </div>
 
-        <button
-          class="bg-white border-black border-2"
-          @click="setScrollType('none')"
-        >
-          No scroll
-        </button>
-      </div>
+          <h4>Scroll speed (time per result line)</h4>
+          <div class="flex items-center mb-2">
+            <label
+              for="scroll-speed"
+              class="block mb-1 mr-2 text-sm font-medium text-gray-900 dark:text-white"
+              >{{ settingsStore.readLineTimeSeconds }}s</label
+            >
+            <input
+              v-model="settingsStore.readLineTimeSeconds"
+              id="scroll-speed"
+              type="range"
+              min="0.1"
+              max="2"
+              step="0.1"
+              class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
--- packages/mrb-client/src/stores/settings.ts
+++ packages/mrb-client/src/stores/settings.ts
@@ -5,12 +5,12 @@ import { defineStore } from 'pinia'
 type ScrollType = 'none' | 'continues' | 'page'
 
 export const useSettingStore = defineStore('settings', () => {
-  const scrollType: Ref<ScrollType> = ref('none')
+  const scrollType: Ref<ScrollType> = ref('page')
   const readLineTimeSeconds = ref(0.2)
   const readLineTimeMS = computed(() => readLineTimeSeconds.value * 1000)
   function setScrollType(type: ScrollType) {
     scrollType.value = type
   }
 
-  return { scrollType, readLineTimeMS, setScrollType }
+  return { scrollType, readLineTimeSeconds, readLineTimeMS, setScrollType }
 })
