8d5f81d6d87fcd31d925acd957d89e93014d26c7
Jan Shanel <chcjohnie@gmail.com>
2023-03-06 19:39:34 +0100

    Refactor competition list into sublist
    Add sublist tests

--- packages/mrb-client/src/components/CompetitionList.vue
+++ packages/mrb-client/src/components/CompetitionList.vue
@@ -1,28 +1,8 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { RouterLink } from 'vue-router'
-
+import CompetitionSublist from './CompetitionSublist.vue'
 import { useCompetitions } from '@/composables/useCompetitions'
 
 const { competitions, competitionsByPeriod } = useCompetitions()
-
-const pagination = ref({
-  past: 5,
-  future: 5,
-})
-
-const paginate = (period: 'past' | 'future', full: boolean) => {
-  if (full) pagination.value[period] = competitionsByPeriod.value[period].length
-  else pagination.value[period] += 5
-}
-
-const competitionsPaginated = computed(() => {
-  const { past, future } = pagination.value
-  return {
-    past: competitionsByPeriod.value.past.slice(0, past),
-    future: competitionsByPeriod.value.future.slice(0, future),
-  }
-})
 </script>
 
 <template>
@@ -31,106 +11,27 @@ const competitionsPaginated = computed(() => {
   </h2>
   <div v-if="competitions" class="my-4">
     <section data-testId="competitions-today">
-      <h3 class="text-xl font-bold text-header">LIVE today</h3>
-      <ul>
-        <li
-          v-for="competition in competitionsByPeriod.today"
-          :key="competition.id"
-          class="py-1"
-        >
-          <RouterLink
-            data-testId="competition-link"
-            class="text-lg hover:font-bold hover:underline"
-            :to="{
-              name: 'event',
-              params: { competitionId: competition.id },
-            }"
-          >
-            {{ competition.name }}
-          </RouterLink>
-        </li>
-      </ul>
+      <CompetitionSublist :competitions="competitionsByPeriod.today">
+        LIVE today
+      </CompetitionSublist>
     </section>
 
     <section class="my-4" data-testId="competitions-future">
-      <h3 class="text-xl font-bold text-header">Upcoming</h3>
-      <ul>
-        <li
-          v-for="competition in competitionsPaginated.future"
-          :key="competition.id"
-          class="py-1"
-        >
-          <RouterLink
-            data-testId="competition-link"
-            class="text-lg hover:font-bold hover:underline-dashed"
-            :to="{
-              name: 'event',
-              params: { competitionId: competition.id },
-            }"
-          >
-            ({{ competition.date }}) {{ competition.name }}
-          </RouterLink>
-        </li>
-      </ul>
-      <div
-        v-show="
-          competitionsByPeriod.future.length !==
-          competitionsPaginated.future.length
-        "
+      <CompetitionSublist
+        :competitions="competitionsByPeriod.future"
+        :is-paginated="true"
       >
-        <button
-          class="text-male font-bold p-1 m-2 rounded outline outline-2 outline-male hover:font-bold hover:bg-slate-300"
-          @click="() => paginate('future', false)"
-        >
-          Show more
-        </button>
-        <button
-          class="text-male font-bold p-1 m-2 rounded outline outline-2 outline-male hover:font-bold hover:bg-slate-300"
-          @click="() => paginate('future', true)"
-        >
-          Show full
-        </button>
-      </div>
+        Upcoming
+      </CompetitionSublist>
     </section>
 
     <section data-testId="competitions-past">
-      <h3 class="text-xl font-bold text-header">Past</h3>
-      <ul>
-        <li
-          v-for="competition in competitionsPaginated.past"
-          :key="competition.id"
-          class="py-1"
-        >
-          <RouterLink
-            data-testId="competition-link"
-            class="text-lg hover:font-bold hover:underline-dashed"
-            :to="{
-              name: 'event',
-              params: { competitionId: competition.id },
-            }"
-          >
-            ({{ competition.date }}) {{ competition.name }}
-          </RouterLink>
-        </li>
-      </ul>
-      <div
-        v-show="
-          competitionsByPeriod.past.length !== competitionsPaginated.past.length
-        "
+      <CompetitionSublist
+        :competitions="competitionsByPeriod.past"
+        :is-paginated="true"
       >
-        <button
-          class="text-male font-bold p-1 m-2 rounded outline outline-2 outline-male hover:font-bold hover:bg-slate-300"
-          @click="() => paginate('past', false)"
-        >
-          Show more
-        </button>
-        <button
-          class="text-male font-bold p-1 m-2 rounded outline outline-2 outline-male hover:font-bold hover:bg-slate-300"
-          @click="() => paginate('past', true)"
-        >
-          Show full
-        </button>
-      </div>
+        Past
+      </CompetitionSublist>
     </section>
   </div>
   <div v-else>Loading events</div>
---
+++ packages/mrb-client/src/components/__tests__/CompetitionSublist.spec.ts
@@ -0,0 +1,90 @@
+import { render, fireEvent } from '@testing-library/vue'
+import { createRouter, createWebHistory, type Router } from 'vue-router'
+
+import CompetitionSublist from '../CompetitionSublist.vue'
+import { DEFAULT_TEST_SIMPLE_COMPETITION } from '@/composables/providers/useMockData'
+
+const TEST_COMPETITIONS_3_ITEMS = new Array(3).fill({}).map((_, i) => ({
+  ...DEFAULT_TEST_SIMPLE_COMPETITION,
+  id: i + 1,
+  name: `TEST_EVENT ${i}`,
+}))
+const TEST_COMPETITIONS_20_ITEMS = new Array(20).fill({}).map((_, i) => ({
+  ...DEFAULT_TEST_SIMPLE_COMPETITION,
+  id: i + 1,
+  name: `TEST_EVENT ${i}`,
+}))
+
+let router: Router
+beforeEach(async () => {
+  router = createRouter({
+    history: createWebHistory(),
+    routes: [
+      { path: '/', name: 'home', component: { template: '<span></span>' } },
+      {
+        path: '/event/:competitionId?',
+        name: 'event',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/CompetitionView.vue'),
+      },
+    ],
+  })
+
+  router.push('/')
+  await router.isReady()
+})
+
+describe('CompetitionSublist', () => {
+  it('should render competitions', () => {
+    const { getAllByTestId, getAllByRole, getByText } = render(
+      CompetitionSublist,
+      {
+        props: { competitions: TEST_COMPETITIONS_3_ITEMS },
+        global: {
+          plugins: [router],
+        },
+        slots: {
+          default: 'TEST LIST',
+        },
+      }
+    )
+
+    expect(getAllByTestId('competition-link')).toHaveLength(3)
+    expect(getAllByRole('listitem')).toHaveLength(3)
+    expect(getAllByRole('link')).toHaveLength(3)
+
+    const oneEventLink = getByText(TEST_COMPETITIONS_3_ITEMS[0].name, {
+      exact: false,
+    })
+    expect(oneEventLink).toHaveAttribute('href', '/event/1')
+
+    expect(getByText('TEST LIST')).toBeInTheDocument()
+  })
+
+  it('should render competitions with pagination', async () => {
+    const { getAllByTestId, getByRole } = render(CompetitionSublist, {
+      props: { competitions: TEST_COMPETITIONS_20_ITEMS, isPaginated: true },
+      global: {
+        plugins: [router],
+      },
+      slots: {
+        default: 'TEST LIST',
+      },
+    })
+
+    expect(getAllByTestId('competition-link')).toHaveLength(5)
+    const moreButton = getByRole('button', { name: 'Show more' })
+    const fullButton = getByRole('button', { name: 'Show full' })
+
+    await fireEvent.click(moreButton)
+    expect(getAllByTestId('competition-link')).toHaveLength(10)
+
+    await fireEvent.click(fullButton)
+    expect(getAllByTestId('competition-link')).toHaveLength(20)
+
+    expect(() => getByRole('button', { name: 'Show more' })).toThrow()
+    expect(() => getByRole('button', { name: 'Show full' })).toThrow()
+  })
+})
